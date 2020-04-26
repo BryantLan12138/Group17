3e4822b64af8aeced5d51d3efdc6d3072fcf2e90
BryantLan12138 <48848298+BryantLan12138@users.noreply.github.com>
2020-04-27 00:15:44 +1000

    change

--- googlemapsapi/resources/views/layouts/app.blade.php
+++ googlemapsapi/resources/views/layouts/app.blade.php
@@ -150,7 +150,7 @@
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+                Carabc
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
